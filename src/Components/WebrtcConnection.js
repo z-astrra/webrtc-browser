5b187e30b5b41d8580bd6a48a0eb98cdb93b84d4
Zephyr Astra <this.zephyr@gmail.com>
2021-11-03 11:46:12 -0700

    first commit

---
+++ src/Components/WebrtcConnection.js
@@ -0,0 +1,14 @@
+function WebrtcConnection() {
+    return (
+        <div>
+            <h1 style={headerStyle}>WebRTC Example for Browser</h1>
+        </div>
+    )
+}
+
+const headerStyle = {
+    color: 'red',
+
+}
+
+export default WebrtcConnection
